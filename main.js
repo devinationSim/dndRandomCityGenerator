545fb77a936e9c8754f182f1a5caa0f1c320251e
[devinationSim] <[sniccoop12@gmail.com]>
2023-06-10 00:43:09 -0700

    OOP changes

--- main.js
+++ main.js
@@ -12,57 +12,74 @@
         //Large City: 5001-25000
         //Metropolis: 25001-75000
 
+//Code to remember
 
 //Math.floor(Math.random() * (max-min + 1)) = min
 // let smallCity = Math.floor(Math.random() * (12000-5001) + 5001)
-
 // console.log(smallCity)
 // settlement, settlement size, population
+
+//console.log(Object.values(objectName))
+//console.log(Object.keys(objectName))
 
 //Each time a new settlement is made, the array in the settlement class should choose between the four population types. Once that type is chosen, the properties of that type should give random values. So that means that the settlement class array should each be a keyword object such as town, city, etc, and whenever that keyword is chosen it will spit out the name, type, size, and population and display it one the DOM
 
 //When the user clicks a button a new settlement object should be made
 
-class SettlementType {
-    constructor(name, settlementSize, population){
-        this.name = name
-        this.settlementType = settlementType
-        this.settlementSize = settlementSize
-        this.population = population
-    }
 
-    static generateRandomName(){
 
-    }
 
-    static generateRandomSettlementSize(){
 
-    }
 
-    static generateRandomPopulation(){
-        
-    }
-}
+
 
 class Settlement {
-    constructor(){
+    constructor(name, settlementTypeArray, settlementSize, population){
+        this._name = name
         this._settlementTypeArray = [
             new SettlementType('thorp', 20-80, )]
+        this._settlementSize = settlementSize
+        this._population = population
     }
 
-    get settlementType(){
-        return this._settlementType
+    class SettlementName extends Settlement { //Use a Markov Chain and Prefixes, Suffixes, and Syllables
+        constructor(){
+            this._settlementName = settlementName
+        }
+    }
+    class SettlementType  { //Use Code down below
+        constructor(settlementType){
+            this._settlementType = settlementType
+        }
+
+        get settlementType(){
+            return this._settlementType
+        }
+
+        get randomSettlement(){
+            return this.randomSettlement()
+        }
+
+        randomSettlement(){
+            let settlementIndex = Math.floor(Math.random() * this._settlementTypeArray.length)
+            let randomSettlementType = this._settlementType[settlementIndex]
+            return document.querySelector('#test').innerText = randomSettlementType
+        }
+
     }
 
-    get randomSettlement(){
-        return this.randomSettlement()
+    class SettlementSize{
+        constructor(){
+            this._settlementSize = settlementSize
+        }
     }
 
-    randomSettlement(){
-       let settlementIndex = Math.floor(Math.random() * this._settlementTypeArray.length)
-        let randomSettlementType = this._settlementType[settlementIndex]
-        return document.querySelector('#test').innerText = randomSettlementType
+    class Population{
+        constructor(){
+            this.population = this.population
+        }
     }
+    
    
 }
 
